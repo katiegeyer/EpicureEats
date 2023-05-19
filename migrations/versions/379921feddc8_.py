a002314fce25b3c0c36448eff50ed018990f3402
Katie Geyer <keg233@nau.edu>
2023-05-19 09:12:44 -0700

    migrations for render

--- migrations/versions/379921feddc8_.py
+++ migrations/versions/379921feddc8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 988276c0d7cd
+Revision ID: 379921feddc8
 Revises: 
-Create Date: 2023-05-19 08:32:54.744807
+Create Date: 2023-05-19 09:12:21.506295
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '988276c0d7cd'
+revision = '379921feddc8'
 down_revision = None
 branch_labels = None
 depends_on = None
