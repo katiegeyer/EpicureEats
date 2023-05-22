94850e2a7ea1443d83549982742ebe44a19622be
Katie Geyer <keg233@nau.edu>
2023-05-22 14:45:37 -0700

    switching fetch back

--- react-app/src/store/ingredients.js
+++ react-app/src/store/ingredients.js
@@ -9,7 +9,7 @@ const DELETE_INGREDIENT = 'ingredients/DELETE_INGREDIENT'
 //         const cookies = document.cookie.split(';');
 //         for (let i = 0; i < cookies.length; i++) {
 //             const cookie = cookies[i].trim();
-//             // Does this cookie string begin with the name we want?
+
 //             if (cookie.substring(0, name.length + 1) === (name + '=')) {
 //                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
 //                 break;
