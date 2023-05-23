5f9658051dbfd9710511affb4a686d9aa5448ae8
Katie Geyer <keg233@nau.edu>
2023-05-23 11:56:47 -0700

    alembic

---
+++ migrations/versions/988276c0d7cd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 988276c0d7cd
+Revises: b99e852cff02
+Create Date: 2023-05-23 11:56:24.460978
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '988276c0d7cd'
+down_revision = 'b99e852cff02'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
