575e03a98ea8500c3c43fc453afbe3732c9890ed
Katie Geyer <keg233@nau.edu>
2023-05-23 13:24:09 -0700

    added missing revision

---
+++ migrations/versions/3737fdd5eecd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3737fdd5eecd
+Revises: d8c82c797313
+Create Date: 2023-05-23 13:23:35.935835
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3737fdd5eecd'
+down_revision = 'd8c82c797313'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
