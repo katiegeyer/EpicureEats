a48479c8c7064156210e25aaa515788de78b7a84
Katie Geyer <keg233@nau.edu>
2023-05-23 11:28:46 -0700

    and agani

--- migrations/versions/988276c0d7cd_.py
+++ migrations/versions/988276c0d7cd_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '988276c0d7cd'
-down_revision = '02d721e971f7'
+down_revision = None
 branch_labels = None
 depends_on = None
 
