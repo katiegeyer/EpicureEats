d11691dd3c4885ff2750b2881cca808df48336ca
Katie Geyer <keg233@nau.edu>
2023-05-25 15:41:28 -0700

    added revision

---
+++ migrations/versions/3737fdd5eecd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3737fdd5eecd
+Revises: 60af3e5a7a4c
+Create Date: 2023-05-25 15:35:57.169767
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3737fdd5eecd'
+down_revision = '60af3e5a7a4c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
